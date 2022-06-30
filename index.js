c506c5c2d1bf603ed2fb0e818d0a5d352d2a5668
niiyazobug <niiyaz5555@gmail.com>
2022-06-30 20:12:57 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ptdjh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
-       
+        await client.connect()
+        const allToDoCollection = client.db("daily-todo-data-collection").collection("all-todo");
+
+
     }
     finally { }
 }
